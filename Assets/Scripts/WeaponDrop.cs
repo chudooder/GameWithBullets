0c8211554866e59402e709c7765b5973413cbb14
Your Name <chenyang.wang123@gmail.com>
2016-10-10 17:47:37 -0500

    Added weapon spawns

--- Assets/Scripts/WeaponDrop.cs
+++ Assets/Scripts/WeaponDrop.cs
@@ -8,6 +8,8 @@ public class WeaponDrop : MonoBehaviour {
 		if (collider.CompareTag ("Hurtbox")) {
 			PlayerWeapons pweapons = collider.GetComponentInParent<PlayerWeapons> ();
 			pweapons.Equip (weapon);
+			GetComponentInParent<WeaponSpawn> ().PickedUp ();
+			Debug.LogFormat ("{0} picked up a {1}", pweapons.name, weapon.UIName);
 			Destroy (gameObject);
 		}
 	}
---
+++ Assets/Scripts/WeaponSpawn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponSpawn : MonoBehaviour
+{
+	public WeaponDrop dropPrefab;
+	public Weapon[] weapons;
+
+	public float frequency;
+	public float deviation;
+
+	void Start(){
+		float next = RandomFromDistribution.RandomNormalDistribution (frequency / 2, deviation / 2);
+		Invoke ("CreateDrop", next);
+	}
+
+	public void PickedUp(){
+		float next = RandomFromDistribution.RandomNormalDistribution (frequency, deviation);
+		Invoke ("CreateDrop", next);
+	}
+
+	private void CreateDrop(){
+		WeaponDrop drop = Instantiate (dropPrefab);
+		drop.transform.SetParent (transform, false);
+		drop.weapon = weapons[Random.Range(0, weapons.Length - 1)];
+	}
+}
+
